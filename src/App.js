20449ee63e74203f01f3910a1101e02b33c8ee0c
Joe <itsjoemcbride@gmail.com>
2022-09-14 21:52:02 +0100

    Configure React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import DrinkDetail from './components/DrinkDetail/DrinkDetail';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className='App'>
+      <Router>
+        <Header></Header>
+        <Route path="/" component={Home} />
+        <Route path="drink/:drinkID" component={DrinkDetail} />
+        <Route component={PageNotFound} />
+        <Footer />
+      </Router>
     </div>
   );
 }
