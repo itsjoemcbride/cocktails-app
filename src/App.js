4b1b098e8aaca9934c0068fbbb0b8959c73515af
Joe <itsjoemcbride@gmail.com>
2022-09-14 23:03:30 +0100

    Refactor React Router for React V6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DrinkDetail from './components/DrinkDetail/DrinkDetail';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import PageNotFound from './components/PageNotFound/PageNotFound';
+import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Router>
         <Header></Header>
-        <Route path="/" component={Home} />
-        <Route path="drink/:drinkID" component={DrinkDetail} />
-        <Route component={PageNotFound} />
+        <div className='container'>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="drink/:drinkID" element={<DrinkDetail />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </div>
