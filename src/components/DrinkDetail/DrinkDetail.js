9e21e544a2b65849f489fb3c095cd5fba4990ad2
Joe <itsjoemcbride@gmail.com>
2022-09-16 23:09:05 +0100

    Add full ingredient info to DrinkDetail
    - not DRY, but it works

--- src/components/DrinkDetail/DrinkDetail.js
+++ src/components/DrinkDetail/DrinkDetail.js
@@ -21,6 +21,7 @@ export default function DrinkDetail(props) {
 
   if (data) {
     const drink = data.drinks[0];
+
     return (
       <div className="details-content">
         <h1>{drink.strDrink}</h1>
@@ -31,7 +32,55 @@ export default function DrinkDetail(props) {
             <p>{`Category: ${drink.strCategory}`}</p>
             <p>{`Info: ${drink.strAlcoholic}`}</p>
             <p>{`Glass: ${drink.strGlass}`}</p>
-            <p>{`Ingredients: ${drink.strIngredient1}`}</p>
+            <p>{`Ingredients:`}</p>
+            {/* figure out how to and then implement a for loop here */}
+            <div className="ingredient-list">
+              <p>
+                {drink.strMeasure1} {drink.strIngredient1}
+              </p>
+              <p>
+                {drink.strMeasure2} {drink.strIngredient2}
+              </p>
+              <p>
+                {drink.strMeasure3} {drink.strIngredient3}
+              </p>
+              <p>
+                {drink.strMeasure4} {drink.strIngredient4}
+              </p>
+              <p>
+                {drink.strMeasure5} {drink.strIngredient5}
+              </p>
+              <p>
+                {drink.strMeasure6} {drink.strIngredient6}
+              </p>
+              <p>
+                {drink.strMeasure7} {drink.strIngredient7}
+              </p>
+              <p>
+                {drink.strMeasure8} {drink.strIngredient8}
+              </p>
+              <p>
+                {drink.strMeasure9} {drink.strIngredient9}
+              </p>
+              <p>
+                {drink.strMeasure10} {drink.strIngredient10}
+              </p>
+              <p>
+                {drink.strMeasure11} {drink.strIngredient11}
+              </p>
+              <p>
+                {drink.strMeasure12} {drink.strIngredient12}
+              </p>
+              <p>
+                {drink.strMeasure13} {drink.strIngredient13}
+              </p>
+              <p>
+                {drink.strMeasure14} {drink.strIngredient14}
+              </p>
+              <p>
+                {drink.strMeasure15} {drink.strIngredient15}
+              </p>
+            </div>
             <p>{`Instructions: ${drink.strInstructions}`}</p>
           </div>
         </div>
